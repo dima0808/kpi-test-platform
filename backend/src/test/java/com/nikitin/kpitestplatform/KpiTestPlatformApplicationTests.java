7cea3d47ccb8838f7541f6aa32de6696db20b611
dimitri <mamchenko2210@gmail.com>
2024-11-01 16:06:07 +0100

    init backend

---
+++ backend/src/test/java/com/nikitin/kpitestplatform/KpiTestPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikitin.kpitestplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpiTestPlatformApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
