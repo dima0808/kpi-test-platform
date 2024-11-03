f44e426d8637287c01da02694bd2feaa3ae73f68
protein2005 <alexeysokole@gmail.com>
2024-11-03 16:01:30 +0200

    install libraries, add styles, and implement sidebar layout

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidebar from '../components/Sidebar';
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default Home;
